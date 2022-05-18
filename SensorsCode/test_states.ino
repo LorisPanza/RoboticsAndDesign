4bb8c53e817a975fff1cd86bb3a8360291147b12
giuseppe-chiari <48725186+giuseppe-chiari@users.noreply.github.com>
2022-05-18 10:57:28 +0200

    Add files via upload

--- SensorsCode/test_states.ino
+++ SensorsCode/test_states.ino
@@ -14,24 +14,39 @@ MPU6050 mpu6050(Wire);
 
 
 long timer = 0;
-int mean, old_mean;
 
+// Capacitive sensor params
 int ref0, ref1;
 static int value_off = 0;
 bool flag_first_time = true;
-
-const int soundPin = A0;
-
-
+int mean, old_mean;
 int mean_offset = 30;
+
+// Big noise sensor params
+const int soundPin = A0;
+int Digital_Pin = 3;
+
+// Sad still state parameters
+long sad_still_timer = 0;
+struct states{
+  bool happy;
+  bool sad;
+  bool angry;
+  bool fear; 
+  };
+
+
+states state = {0,0,0,0};
 
 StateMachine machine = StateMachine();
 
 
+// States declarations
 State* calibration = machine.addState(&state_calibration); //state 0
 State* still_neutral = machine.addState(&state_still_neutral); //state 1
 State* shoulder = machine.addState(&state_shoulder); //state 2
 State* fear = machine.addState(&state_fear); //state 3
+State* sad_still = machine.addState(&state_sad_still); //stato 4
 
 
 void setup() {
@@ -39,7 +54,8 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  pinMode(soundPin,INPUT);  
+  pinMode(soundPin,INPUT);
+  pinMode(Digital_Pin, INPUT);
   
   //mpu6050.begin();
   //mpu6050.calcGyroOffsets(true);
@@ -51,11 +67,12 @@ void setup() {
   calibration->addTransition(&transition_calib_still_neutral, still_neutral);
 
   // Transitions exiting shoulder state
-  shoulder->addTransition(&transition_shoulder_still, calibration);
+  shoulder->addTransition(&transition_shoulder_still, still_neutral);
   shoulder->addTransition(&transition_shoulder_big_noise, fear);
 
   // Transitions exiting still_neutral state
-  still_neutral->addTransition(&transition_still_calib, calibration);
+  still_neutral->addTransition(&transition_still_shoulder, shoulder);
+  still_neutral->addTransition(&transition_still_sad_still, sad_still);
 
 }
 
@@ -85,6 +102,10 @@ void getMeanProximity(){
   
   mean = total_value / count;
 
+  if(old_mean == 0){
+    old_mean = mean;
+  }
+
   Serial.print("MEAN: ");Serial.print(mean);
   Serial.println('\n');
 }
@@ -94,6 +115,7 @@ bool checkOverMeanProximity(){
     
   if(mean > old_mean + mean_offset){
     old_mean = mean;
+    Serial.print("STILL-SHOULDER - ");Serial.print("OLD MEAN: ");Serial.print(old_mean);Serial.print(" MEAN: ");Serial.println(mean);
     return true;
   }
 
@@ -106,14 +128,13 @@ bool checkBelowMeanProximity(){
     
   if(mean < old_mean - mean_offset){
     old_mean = mean;
+    Serial.print("SHOULDER-STILL - ");Serial.print("OLD MEAN: ");Serial.print(old_mean);Serial.print(" MEAN: ");Serial.println(mean);
     return true;
   }
 
   return false;
 
 }
-
-
 
 void loop(){
   // put your main code here, to run repeatedly:
@@ -136,6 +157,10 @@ void state_calibration(){
   else{
     machine.transitionTo(still_neutral);
   }
+
+  if(flag_first_time){
+    old_mean = mean;
+  }
   
   flag_first_time = false;
   
@@ -143,23 +168,22 @@ void state_calibration(){
 
 void state_still_neutral(){
 
+  sad_still_timer++;
   Serial.println("I AM ON THE TABLE");
-
-  
-  
 }
 
 void state_shoulder(){
 
   Serial.println("I AM ON THE SHOULDER");
-  
 }
 
 void state_fear(){
 
   Serial.println("FEAR");
+}
 
-  
+void state_sad_still(){
+  Serial.println("WAITING TO BE PICKED UP");
 }
 
 bool transition_shoulder_big_noise(){
@@ -172,21 +196,29 @@ bool transition_shoulder_big_noise(){
 
   val = val - sound_offset;
 
+  int Digital = digitalRead(Digital_Pin);
+
+  Serial.print("NOISE VAL: ");Serial.println(val);
+  Serial.print("Limit :");Serial.println(Digital);
+
+  
+
   if(val > big_sound_th){
-    return true;
+    return false;
   }
 
   return false;
 }
 
-bool transition_calib_shoulder(){
 
+bool transition_calib_shoulder(){
+  
   return checkOverMeanProximity();
 
 }
 
 bool transition_calib_still_neutral(){
-
+  
   return checkBelowMeanProximity();
   
 }
@@ -197,8 +229,72 @@ bool transition_shoulder_still(){
   
 }
 
-bool transition_still_calib(){
+bool transition_still_shoulder(){
 
   return checkOverMeanProximity();
   
+}
+
+bool transition_still_sad_still(){
+
+  if(sad_still_timer > 10){
+    Serial.println("Sono passati 5 secondi");
+    set_sad();
+    display_state();
+    //é triste per sempre fin quando non viene preso
+    sad_still_timer = 0;
+    return true;
+  }
+
+  return false;
+  
+}
+
+void display_state(){
+
+  if(state.happy){
+    Serial.println("Happy");
+  }
+
+  else if(state.sad){
+    Serial.println("Sad");
+  }
+
+  else if(state.angry){
+    Serial.println("Angry");
+  }
+
+  else if(state.fear){
+    Serial.println("Fear");
+  }
+
+  else{
+    Serial.println("Neutral");
+    return;      
+  }
+
+  delay(5000);
+
+   
+}
+
+
+void set_neutral(){ 
+  state ={0,0,0,0};
+}
+
+void set_happy(){
+  state={1,0,0,0};
+}
+
+void set_angry(){
+  state={0,0,1,0};
+}
+
+void set_fear(){
+  state={0,0,0,1};
+}
+
+void set_sad(){
+  state={0,1,0,0};
 }
